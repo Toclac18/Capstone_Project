19bd99c4c5a19e775c70df49f89f219ed218e0fd
Quangz <quangzdrive>
2025-11-25 23:18:06 +0700

    [be] feat: BA tag management

--- be/src/main/java/com/capstone/be/repository/TagRepository.java
+++ be/src/main/java/com/capstone/be/repository/TagRepository.java
@@ -10,12 +10,14 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TagRepository extends JpaRepository<Tag, UUID> {
+public interface TagRepository extends JpaRepository<Tag, UUID>,
+    JpaSpecificationExecutor<Tag> {
 
   Optional<Tag> findByName(String name);
 
